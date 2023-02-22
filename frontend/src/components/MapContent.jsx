fc01e4b1b660cba44d0d927a4a82356882d873fa
MarcelHor <mar.horv22@gmail.com>
2023-02-22 22:56:00 +0100

    Clusters

--- frontend/src/components/MapContent.jsx
+++ frontend/src/components/MapContent.jsx
@@ -8,10 +8,10 @@ export default function MapContent(props){
     //create icon for cluster
     const createClusterCustomIcon = function (cluster) {
         const markers = cluster.getAllChildMarkers();
-        const childMarkers = markers.map(marker => marker.options.children.props.children.props.children[0].props.children);
+        const childMarkers = markers.map((marker) => marker.options.children.props.children.props.children[0].props.children);
         // Count the number of markers in each category
         const categories = {};
-        childMarkers.forEach(marker => {
+        childMarkers.forEach((marker) => {
             const category = marker.replace(/\s+/g, '');
             categories[category] = categories[category] ? categories[category] + 1 : 1;
         });
@@ -23,46 +23,49 @@ export default function MapContent(props){
             výbuchy: '#33ff33',
             Dopravnínehody: '#3388ff',
             KrádežePodvody: '#9933ff',
-            Zbraně: "#ff33ff",
+            Zbraně: '#ff33ff',
             Jinámajetková: '#ff3333',
             Toxikománie: '#ff9933',
         };
 
-        // Define the radius and line width for the pie chart
-        const radius = 30;
+        // Define the radius and line width for the ring chart
+        const radius = 25;
         const lineWidth = 10;
 
         // Create the canvas element and context
-        const canvas = document.createElement("canvas");
+        const canvas = document.createElement('canvas');
         canvas.width = radius * 2;
         canvas.height = radius * 2;
-        const ctx = canvas.getContext("2d");
+        const ctx = canvas.getContext('2d');
 
-        let startAngle = 0;
+        // Calculate total number of markers and total width of the ring chart
+        const markerCount = childMarkers.length;
+        const totalWidth = lineWidth * markerCount;
+
+        // Draw the ring chart
+        let startAngle = -Math.PI / 2;
         for (const [category, count] of Object.entries(categories)) {
-            const endAngle = startAngle + (count / childMarkers.length) * 2 * Math.PI;
-            ctx.fillStyle = categoryColors[category];
-            console.log(categoryColors[category]);
+            const endAngle = startAngle + (count / markerCount) * 2 * Math.PI;
+            ctx.strokeStyle = categoryColors[category];
+            ctx.lineWidth = lineWidth;
             ctx.beginPath();
-            ctx.moveTo(radius, radius);
-            ctx.arc(
-                radius,
-                radius,
-                radius - lineWidth / 2,
-                startAngle,
-                endAngle
-            );
-            ctx.closePath();
-            ctx.fill();
+            ctx.arc(radius, radius, radius - lineWidth / 2, startAngle, endAngle);
+            ctx.stroke();
             startAngle = endAngle;
         }
 
         // Create the div icon with the canvas element and marker count label
-        const markerCount = childMarkers.length;
-        const iconDiv = L.DomUtil.create("div");
+        const iconDiv = document.createElement('div');
+        iconDiv.style.position = 'relative';
+        iconDiv.style.width = `${radius * 2}px`;
+        iconDiv.style.height = `${radius * 2}px`;
+        iconDiv.innerHTML = `<div style="position: absolute; top: 50%; left: 50%; transform: translate(-65%, -45%); font-size: 15px; font-weight: bold;">${markerCount}</div><canvas></canvas>`;
+        iconDiv.querySelector('canvas').getContext('2d').drawImage(canvas, 0, 0);
+
+        // Set the canvas as the icon's HTML
         const icon = L.divIcon({
-            html: canvas,
-            className: "marker-cluster",
+            html: iconDiv,
+            className: 'marker-cluster',
             iconSize: L.point(radius * 2, radius * 2),
             iconAnchor: L.point(radius, radius),
         });
@@ -70,13 +73,31 @@ export default function MapContent(props){
         return icon;
     };
 
+    const createMarkerCustomIcon = function (color) {
+        const iconDiv = document.createElement('div');
+        iconDiv.style.backgroundColor = color;
+        iconDiv.style.width = '30px';
+        iconDiv.style.height = '30px';
+        iconDiv.style.borderRadius = '50%';
+        iconDiv.style.boxShadow = '0px 0px 6px rgba(0, 0, 0, 0.5)';
+        iconDiv.innerHTML = `<div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 14px; font-weight: bold; color: white;">1</div>`;
+        const icon = L.divIcon({
+            html: iconDiv,
+            className: 'marker-custom',
+            iconSize: L.point(30, 30),
+            iconAnchor: L.point(15, 15),
+        });
+        return icon;
+    };
+
 
     return (
         <>
             <MarkerClusterGroup showCoverageOnHover={false}
-                                iconCreateFunction={createClusterCustomIcon}>
+                                iconCreateFunction={createClusterCustomIcon}
+            >
                 {locations.map((location, index) => (
-                        <Marker key={index} position={[location.coordinates[1], location.coordinates[0]]}>
+                        <Marker key={index} position={[location.coordinates[1], location.coordinates[0]]} icon={createMarkerCustomIcon('#ff3333')}>
                             <Popup>
                                 <div>
                                     <h1>{location.properties.crime.name}</h1>
