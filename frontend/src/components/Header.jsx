6c6b43385c1b9672d62dd8e2a0a97033f42386e2
Marcel_Horvath <marcel.horvath@tul.cz>
2023-05-08 20:04:31 +0200

    header popup

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,8 +5,9 @@ import Popup from './HeaderPopup';
 export default function Header() {
     const [popup, setPopup] = useState(null);
 
-    const openPopup = (name, description) => {
-        setPopup({name, description});
+    const openPopup = (name) => {
+        console.log(name);
+        setPopup({name});
     };
 
     const closePopup = () => {
@@ -16,48 +17,62 @@ export default function Header() {
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 
-    return (
-        <header className="flex flex-wrap justify-between items-center py-2 px-4 md:px-8 bg-header text-white">
-            <div className="flex items-center w-full md:w-auto justify-between">
-                <a href="/" className="flex items-center">
-                    <img src={logo_s2} alt="logo" className="h-16 mr-2"/>
-                    <h1 className="text-2xl font-bold">MAPA KRIMINALITY LIBEREC</h1>
-                </a>
-                <button
-                    className="block md:hidden focus:outline-none"
-                    onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M4 6h16M4 12h16M4 18h16"/>
-                    </svg>
-                </button>
-            </div>
+    return (<header className="flex flex-wrap justify-between items-center py-2 px-4 md:px-8 bg-header text-white">
+        <div className="flex items-center w-full md:w-auto justify-between">
+            <a href="/" className="flex items-center">
+                <img src={logo_s2} alt="logo" className="h-16 mr-2"/>
+                <h1 className="text-2xl font-bold">MAPA KRIMINALITY LIBEREC</h1>
+            </a>
+            <button
+                className="block md:hidden focus:outline-none"
+                onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                     stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                          d="M4 6h16M4 12h16M4 18h16"/>
+                </svg>
+            </button>
+        </div>
 
-            <nav className={`w-full md:w-auto ${isMobileMenuOpen ? 'block' : 'hidden'} md:block`}>
-                <ul className="flex flex-col md:flex-row items-center md:items-stretch md:justify-end">
-                    <li
-                        className="hover:bg-white hover:bg-opacity-5 rounded p-2 md:mr-4 cursor-pointer"
-                        onClick={() => openPopup('Nápověda', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, quia.')}
-                    >
-                        NÁPOVĚDA
-                    </li>
-                    <li
-                        className="hover:bg-white hover:bg-opacity-5 rounded p-2 md:mr-4 cursor-pointer"
-                        onClick={() => openPopup('O projektu', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, quia.')}
-                    >
-                        O PROJEKTU
-                    </li>
-                    <li
-                        className="hover:bg-white hover:bg-opacity-5 rounded p-2 cursor-pointer"
-                        onClick={() => openPopup('Kontakt', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, quia.')}
-                    >
-                        KONTAKT
-                    </li>
-                </ul>
-            </nav>
-            {popup && <Popup name={popup.name} description={popup.description} closePopup={closePopup}/>}
-        </header>
-    );
+        <nav className={`w-full md:w-auto ${isMobileMenuOpen ? 'block' : 'hidden'} md:block`}>
+            <ul className="flex flex-col md:flex-row items-center md:items-stretch md:justify-end">
+                <li
+                    className="hover:bg-white hover:bg-opacity-5 rounded p-2 md:mr-4 cursor-pointer"
+                    onClick={() => openPopup('Nápověda')}
+                >
+                    NÁPOVĚDA
+                </li>
+                <li
+                    className="hover:bg-white hover:bg-opacity-5 rounded p-2 md:mr-4 cursor-pointer"
+                    onClick={() => openPopup('O projektu')}
+                >
+                    O PROJEKTU
+                </li>
+                <li
+                    className="hover:bg-white hover:bg-opacity-5 rounded p-2 cursor-pointer"
+                    onClick={() => openPopup('Kontakt')}
+                >
+                    KONTAKT
+                </li>
+            </ul>
+        </nav>
+        {popup && (<Popup name={popup.name} closePopup={closePopup}>
+            {popup.name === 'Nápověda' && (<div className="p-4">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultricies
+                </p>
+            </div>)}
+            {popup.name === 'O projektu' && (<div className="p-4">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultricies
+                </p>
+            </div>)}
+            {popup.name === 'Kontakt' && (<div className="p-4">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultricies
+                </p>
+            </div>)}
+        </Popup>)}
+    </header>);
 }
--- frontend/src/components/HeaderPopup.jsx
+++ frontend/src/components/HeaderPopup.jsx
@@ -1,5 +1,6 @@
 import close from '../assets/close.svg'
 import {useEffect} from "react";
+
 export default function HeaderPopup(props) {
 
     const escFunction = (event) => {
@@ -16,13 +17,13 @@ export default function HeaderPopup(props) {
     }, []);
 
 
-    return (
-        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 z-10 text-black">
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-1/2 h-2/3 rounded-lg">
-                <img src={close} onClick={props.closePopup} className="absolute top-0 right-0 m-4 w-8 cursor-pointer" alt="Close button"/>
-                <h1 className="text-2xl font-bold text-center mt-4">{props.name}</h1>
-                {props.description}
-            </div>
+    return (<div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 z-10 text-black">
+        <div
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white sm:w-1/2 w-3/4 h-2/3 rounded-lg">
+            <img src={close} onClick={props.closePopup} className="absolute top-0 right-0 m-4 w-8 cursor-pointer"
+                 alt="Close button"/>
+            <h1 className="text-2xl font-bold text-center mt-4">{props.name}</h1>
+            {props.children}
         </div>
-    )
+    </div>)
 }
