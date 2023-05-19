852307cd0c0e4d7e37b7f8c639e2954262d59c64
Marcel_Horvath <marcel.horvath@tul.cz>
2023-05-19 15:27:52 +0200

    popup summary

--- frontend/src/js/colors.js
+++ frontend/src/js/colors.js
@@ -28,6 +28,22 @@ export const CATEGORY_NAMES = {
         79:"DOPRAVNÍ NEHODY",
 };
 
+export const CATEGORY_COLORS_NAMES = {
+    "NÁSILNÁ TRESTNÁ ČINNOST": '#af0000',
+    "Přestupky": '#ff8000',
+    "POŽÁRY VÝBUCHY ŽIVELNÉ POHROMY":'#ff0000',
+    "KRÁDEŽE": '#007e00',
+    "KRÁDEŽE VLOUPÁNÍM": '#00ffff',
+    "OSTATNÍ MAJETKOVÁ TRESTNÁ ČINNOST": '#00f600',
+    "OBECNĚ NEBEZPEČNÁ TRESTNÁ ČINNOST": '#8000ff',
+    "TOXIKOMÁNIE": '#ff00ff',
+    "TRESTNÁ ČINNOST SE ZBRANÍ": '#eaa500',
+    "EXTREMISMUS": '#ff0000',
+    "PODVODY": '#00a200',
+    "DOPRAVNÍ NEHODY": '#ffff00',
+};
+
+
 export const findParent = (id1, id2)=>{
     //check if id1 or id2 is in the category names if so return its id
     if (CATEGORY_NAMES[id1] || CATEGORY_NAMES[id2]){
--- frontend/src/js/mapItemClick.js
+++ frontend/src/js/mapItemClick.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import calendar from '../assets/calendar.svg';
 import clock from '../assets/clock.svg';
-import {CATEGORY_COLORS, CATEGORY_NAMES, findParent} from './colors';
+import {CATEGORY_COLORS, CATEGORY_COLORS_NAMES, CATEGORY_NAMES, findParent} from './colors';
 import rightArrow from '../assets/right_arrow.svg';
 
 
 export const mapItemClick = (map, markers, position) => {
-    const container = document.createElement('div'), contentContainer = document.createElement('div'),
-        buttonContainer = document.createElement('div'), popupContent = document.createElement('div');
+    const container = document.createElement('div'), contentContainer = document.createElement('div'), buttonContainer = document.createElement('div'), popupContent = document.createElement('div');
     container.appendChild(contentContainer);
     container.appendChild(buttonContainer);
     contentContainer.appendChild(popupContent);
@@ -17,6 +16,8 @@ export const mapItemClick = (map, markers, position) => {
     popupContentContainer.appendChild(popupContent);
     contentContainer.appendChild(popupContentContainer);
 
+    const count = document.createElement('span');
+    count.style.marginLeft = '64px';
 
     const maxPage = 100;
     let markerIds = [];
@@ -95,18 +96,62 @@ export const mapItemClick = (map, markers, position) => {
 
     const updatePopupContent = (direction) => {
         if (data) {
-            const date = new Date(data[index].date);
-            const dateStr = date.toLocaleDateString('cs-CZ', {
-                day: 'numeric', month: 'numeric', year: 'numeric',
-            });
-            const timeStr = date.toLocaleTimeString('cs-CZ', {
-                hour: 'numeric', minute: 'numeric',
-            });
+            if (markers.length > 500) {
 
-            const crime_parent = findParent(data[index].crime_type_id, data[index].crime_type_id2);
-            const categoryColor = CATEGORY_COLORS[crime_parent];
-            const crime_parent_name = CATEGORY_NAMES[crime_parent];
-            popupContent.innerHTML = `
+                const crimeIds = markers.map((item) => item.options.crime_type);
+                const crimeCounts = {}; // Object to store the count of each crime type
+                const crimeNames = crimeIds.map((id) => CATEGORY_NAMES[id]); // Get the names of the crime types
+
+                // Count the occurrences of each crime type
+                crimeNames.forEach((name) => {
+                    crimeCounts[name] = (crimeCounts[name] || 0) + 1;
+                });
+                const maxCount = Math.max(...Object.values(crimeCounts)); // Get the maximum count of a crime type
+
+
+                // Generate the HTML for the graph-like representation
+                let graphHTML = '';
+                Object.entries(crimeCounts).forEach(([name, count]) => {
+                    const percentage = (count / maxCount) * 100;
+                    const barColor = CATEGORY_COLORS_NAMES[name];
+                    graphHTML += `
+                                    <div>
+                                     <div class="lowercase text-left flex justify-between text-xs items-center">
+                                                          <div class="">${name}</div>
+                                                          <div class="">${count}</div>
+                                                        </div>
+                                                        <div class="relative">
+                                        <div class="overflow-hidden h-1 mb-1 text-xs rounded" style="background-color: ${barColor}; width: ${percentage}px;"></div>                     
+                                    </div>                 
+                                `;
+                });
+
+                let total = `<div class="flex justify-between font-semibold"><span>Celkem:</span> <span>${markers.length}</span></div>`;
+
+
+
+                // Set the HTML content of the popup
+                return popupContent.innerHTML = `
+                  <div class="text-center ">
+                    ${graphHTML}
+                    <hr class="my-2">
+                    ${total}
+                  </div>
+                `;
+            } else {
+                const date = new Date(data[index].date);
+                const dateStr = date.toLocaleDateString('cs-CZ', {
+                    day: 'numeric', month: 'numeric', year: 'numeric',
+                });
+                const timeStr = date.toLocaleTimeString('cs-CZ', {
+                    hour: 'numeric', minute: 'numeric',
+                });
+
+                const crime_parent = findParent(data[index].crime_type_id, data[index].crime_type_id2);
+                const categoryColor = CATEGORY_COLORS[crime_parent];
+                const crime_parent_name = CATEGORY_NAMES[crime_parent];
+
+                popupContent.innerHTML = `
             <div class="text-center h-60">
                 <h1 class="lowercase first-letter:uppercase border-b-4 text-center p-2" style="border-color: ${categoryColor};"> ${crime_parent_name}</h1>
                 <div class="flex justify-between">
@@ -134,19 +179,19 @@ export const mapItemClick = (map, markers, position) => {
                 </div>
             </div>
         `;
-            popupContent.animate([// keyframes
-                {
-                    transform: `translateX(${direction > 0 ? '-100%' : '100%'})`, opacity: '0'
-                }, {transform: 'translateX(0)', opacity: '1'}], {
-                // timing options
-                duration: 200, iterations: 1
-            });
-            count.textContent = `(${currentMarkerIndex + 1}/${markers.length})`;
+                popupContent.animate([// keyframes
+                    {
+                        transform: `translateX(${direction > 0 ? '-100%' : '100%'})`, opacity: '0'
+                    }, {transform: 'translateX(0)', opacity: '1'}], {
+                    // timing options
+                    duration: 250, iterations: 1
+                });
+                count.textContent = `(${currentMarkerIndex + 1}/${markers.length})`;
+            }
         }
     }
 
-    // Add buttons to button container div if there is more than one marker
-    if (markers.length > 1) {
+    if (markers.length < 500 && markers.length > 1) {
         const prevButton = document.createElement('button');
         prevButton.innerHTML = `<img src="${rightArrow}" alt="previous" style="width: 24px; height: 24px; rotate: 180deg;" class="rounded-full hover:bg-gray-200">`;
         prevButton.addEventListener('click', () => cycleMarkers(-1));
@@ -157,24 +202,22 @@ export const mapItemClick = (map, markers, position) => {
         nextButton.innerHTML = `<img src="${rightArrow}" alt="next" style="width: 24px; height: 24px;" class="rounded-full hover:bg-gray-200">`;
         nextButton.addEventListener('click', () => cycleMarkers(1));
         buttonContainer.appendChild(nextButton);
+
+
+        count.textContent = `(${currentMarkerIndex + 1}/${markers.length})`;
+
+        if (markers.length > 1) buttonContainer.appendChild(count);
+
+        // Set CSS styles to arrange the content and buttons vertically
+        container.style.display = 'flex';
+        container.style.flexDirection = 'column';
+        container.marginLeft = '100px';
+
+        buttonContainer.style.display = 'flex';
+        buttonContainer.style.justifyContent = 'center';
+        buttonContainer.style.alignItems = 'center';
+        buttonContainer.style.marginTop = '20px';
     }
-
-    const count = document.createElement('span');
-    count.style.marginLeft = '64px';
-    count.textContent = `(${currentMarkerIndex + 1}/${markers.length})`;
-
-    if (markers.length > 1) buttonContainer.appendChild(count);
-
-    // Set CSS styles to arrange the content and buttons vertically
-    container.style.display = 'flex';
-    container.style.flexDirection = 'column';
-    container.marginLeft = '100px';
-
-    buttonContainer.style.display = 'flex';
-    buttonContainer.style.justifyContent = 'center';
-    buttonContainer.style.alignItems = 'center';
-    buttonContainer.style.marginTop = '20px';
-
 
     // Fetch data for the first time
     fetchData(currentPage);
